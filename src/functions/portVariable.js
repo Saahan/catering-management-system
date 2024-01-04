c74c6bca5eca2600fc6472cefb251a41bad85bbf
Saahan <105241497+Saahan@users.noreply.github.com>
2024-01-04 08:38:53 +0530

    Update portVariable.js

--- src/functions/portVariable.js
+++ src/functions/portVariable.js
@@ -2,5 +2,5 @@ const domainName = "https://catering-management-system-api.onrender.com"
 
 export {domainName}
 
-// "https://localhost:5000"
+// "http://localhost:5000"
 // "https://catering-management-system-api.onrender.com"
