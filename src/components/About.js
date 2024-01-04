22d08ab760fb05cc4fad26913bb3879217291593
Saahan Mombasawala <smombasa1989@gmail.com>
2024-01-04 11:58:12 +0530

    update "About" view

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../styles/views.css";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="container">
+      <h1>About</h1> <hr />
+      <img src="/img/field.jpg" className="generic-field" alt="field"></img>
+      <p>
+        The catering management system connects buyers and sellers of food
+        items, including fully cooked dishes as well as raw materials. Products
+        are sold in bulk by weight, while the seller takes care of packaging and
+        delivery. It is our mission to provide an excellent platform for
+        caterers to showcase their products and sell them remotely to clients
+        located all around the country. Happy buying and selling!
+      </p>
+    </div>
+  );
 }
