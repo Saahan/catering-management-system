115f6b1d65d27464dfb29ffe292c7cf1d8796882
Saahan Mombasawala <smombasa1989@gmail.com>
2024-01-04 11:44:57 +0530

    update responsiveness

--- src/functions/portVariable.js
+++ src/functions/portVariable.js
@@ -3,8 +3,4 @@ const domainName = "https://catering-management-system-api.onrender.com"
 export {domainName}
 
 // "http://localhost:5000"
-<<<<<<< HEAD
 // "https://catering-management-system-api.onrender.com"
-=======
-// "https://catering-management-system-api.onrender.com"
->>>>>>> c74c6bca5eca2600fc6472cefb251a41bad85bbf
